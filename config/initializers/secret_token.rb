b198a8d905af09fc8c4555c25f20a5c41ee34a88
V <v@MacBook-Pro-de-V.local>
2014-03-22 22:49:42 -0300

    Blog scaffold, devise and twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KairoscopeSys::Application.config.secret_key_base = '1111ab2de6acd0e5abf61630cb2f6c21998a77c7301d6296a1f6ce396c1243b188150393648c2c0b847b7436bf3f8ec7f99d0725e471e95fc1a4089842bf413a'
