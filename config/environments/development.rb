463aafe78aaaacf0db09c75da4e764133ae93c6b
jamesperet <james.peret@gmail.com>
2014-04-06 16:24:38 -0300

    Embeded font-face futura

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ KairoscopeSys::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.serve_static_assets
   
   I18n.locale = "pt-BR"
   
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,6 @@ KairoscopeSys::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.serve_static_assets
 end
